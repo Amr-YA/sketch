6b1a6eec35b4f8e120d76caafc6ac6ab9ff374c6
Amr-YA <eng.amryasser@gmail.com>
2024-06-16 18:28:08 +0300

    ADD: create grid elements from js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Javascript file loaded")
+
+let grid_size = 16
+
+const sketch_container = document.querySelector(".sketch-container")
+
+
+for (i = 0; i < grid_size; i++) {
+    console.log(i)
+    const grid_row = document.createElement("div")
+    grid_row.classList.add("grid-row")
+    for (j = 0; j < grid_size; j++) {
+        const grid_element = document.createElement("div")
+        grid_element.textContent = `${i}:${j}` // for testing
+        grid_row.appendChild(grid_element)
+    }
+    sketch_container.appendChild(grid_row)
+}
