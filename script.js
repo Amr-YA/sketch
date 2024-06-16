b11508e21fd69f74d6c367fed48cd361fa0e9222
Amr-YA <eng.amryasser@gmail.com>
2024-06-16 18:47:46 +0300

    ADD: js hover affects background color

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let grid_size = 16
 
 const sketch_container = document.querySelector(".sketch-container")
 
+function changeColor(event) {
+    console.log(this)
+    this.classList.add("grid-on")
+}
 
 for (i = 0; i < grid_size; i++) {
     console.log(i)
@@ -11,8 +15,8 @@ for (i = 0; i < grid_size; i++) {
     grid_row.classList.add("grid-row")
     for (j = 0; j < grid_size; j++) {
         const grid_element = document.createElement("div")
-        grid_element.textContent = `${i}:${j}` // for testing
         grid_element.classList.add("grid-element")
+        grid_element.addEventListener('mouseover', changeColor)
         grid_row.appendChild(grid_element)
     }
     sketch_container.appendChild(grid_row)
